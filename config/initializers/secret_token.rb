580fe7253701ff59624aa6627df33bea20e6475f
Patrick McNeal <patrick.mcneal@gmail.com>
2013-01-31 14:56:26 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RandomMatch::Application.config.secret_token = '8bd2f0f4cdae20716d69dc23b69709132240729c2db02ade122367a178fa9958d310ca4e89c06bb5ece5e80156957c248c5a35064c1f772a8244fde9e922b61b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RandomMatch::Application.config.session_store :cookie_store, key: '_randomMatch_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RandomMatch::Application.config.session_store :active_record_store
